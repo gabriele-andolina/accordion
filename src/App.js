468eb3f1f81378e8e734da8b2e39c012b472ff74
gabriele-andolina <gabe.andolina@gmail.com>
2023-09-19 15:45:20 +0200

    Components: Created AccordionItem

--- src/App.js
+++ src/App.js
@@ -26,3 +26,14 @@ export default function App() {
 function Accordion() {
     return <div>TODO</div>;
 }
+
+function AccordionItem(number, title, text) {
+    return (
+        <div className="item">
+            <p className="number">{number}</p>
+            <p className="title">{title}</p>
+            <p className="icon">-</p>
+            <div className="content-box">{text}</div>
+        </div>
+    );
+}
